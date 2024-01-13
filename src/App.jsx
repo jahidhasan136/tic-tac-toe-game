2d4d37554dcf4a516109db9c8588049c6b2c425e
Jahid Hasan <jh3495689@gmail.com>
2024-01-13 09:40:59 +0600

    added all game code

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,17 @@ function Squire({ value, onSquireClick }) {
   );
 }
 
-function Board() {
-  const [squire, setSquire] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+function Board({ squire, onPlay, xIsNext }) {
+  const winner = calculateWinner(squire);
+  let status;
+  if (winner) {
+    status = "Winner:" + winner;
+  } else {
+    status = "Next player:" + (xIsNext ? "X" : "O");
+  }
 
   function handleClick(i) {
-    if (squire[i]) {
+    if (squire[i] || calculateWinner(squire)) {
       return;
     }
     const nextSquire = squire.slice();
@@ -25,11 +30,11 @@ function Board() {
     } else {
       nextSquire[i] = "O";
     }
-    setSquire(nextSquire);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquire);
   }
   return (
     <>
+      <div>{status}</div>
       <div className="flex">
         <Squire value={squire[0]} onSquireClick={() => handleClick(0)} />
         <Squire value={squire[1]} onSquireClick={() => handleClick(1)} />
@@ -49,4 +54,75 @@ function Board() {
   );
 }
 
-export default Board;
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentSquire = history[currentMove];
+
+  function handlePlay(nextSquire) {
+    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquire];
+    setHistory([...history, nextSquire]);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0);
+  }
+
+  const moves = history.map((squires, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to the move # ${move}`;
+    } else {
+      description = `Go to start the game`;
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <div>
+        <Board
+          squire={currentSquire}
+          onPlay={handlePlay}
+          // setSquire={setSquire}
+          xIsNext={xIsNext}
+          // setXIsNext={setXIsNext}
+        />
+      </div>
+      <div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// export default Board;
